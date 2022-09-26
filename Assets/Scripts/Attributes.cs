c3d86a63dda5443e899aaae73d862b29ceefeaf2
Seçkin KESKİN <keskinseckin@gmail.com>
2022-09-26 15:05:22 +0300

    Future: Base class
    Attributes base class was added.

---
+++ Assets/Scripts/Attributes.cs
@@ -0,0 +1,24 @@
+public class Attributes 
+{
+    private int _strength = 1;
+    private int _agility = 1;
+    private int _intelligence = 1;
+
+    public int strength
+    {
+        get => _strength;
+        set => _strength = value;
+    }
+
+    public int agility
+    {
+        get => _agility;
+        set => _agility = value;
+    }
+
+    public int intelligence
+    {
+        get => _intelligence;
+        set => _intelligence = value;
+    }
+}
