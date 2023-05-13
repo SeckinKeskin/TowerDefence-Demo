ccdb85b926c86bc2bf46ba759e9314a173732335
Seçkin KESKİN <keskinseckin@gmail.com>
2023-05-13 12:47:56 +0300

    Refactor: Timer
    Timer class improvment. Properties more clean.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,36 +6,27 @@ public class Timer
 {
     public event Action TimerEnded;
     public event Action TimeChanged;
-    private float _time = 0.0f;
-    private float _target = 0.0f;
-    private float _speed = 1.0f;
-    private string _name = "Timer";
-    public float time{ get => _time; }
-    public float target { get => _target; }
-    public float speed { get => _speed; }
-    public string name { get => _name; set => _name = value; }
+    public float time{ get; private set; }
+    public float target { get; private set; }
+    public float speed { get; private set; }
+    public string name { get; private set; }
 
-/// <summary>
-/// When want to start the timer. First of all, timer should be set it.
-/// </summary>
-/// <param name="timeTarget">Destination point of time</param>
-/// <param name="timeSpeed">Speed of time multiplier</param>
     public void SetTimer(float timeTarget, float timeSpeed = 1.0f)
     {
-        _time = 0;
-        _speed = speed;
-        _target = timeTarget;
+        time = 0;
+        speed = timeSpeed;
+        target = timeTarget;
     }
 
     public IEnumerator StartTimer()
     {
-        float timeLapse = 1.0f * _speed;
-        _time = 0.0f;
+        float timeLapse = 1.0f * speed;
+        time = 0.0f;
 
-        while(_time < _target)
+        while(time < target)
         {
             yield return new WaitForSeconds(timeLapse);
-            _time += timeLapse;
+            time += timeLapse;
             
             TimeChangeHandler();
         }
